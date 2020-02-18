a93d6835af5870e5ed6fdf11f6603c780bdb576a
khristenko <khigor777@yandex.ru>
2020-02-18 12:28:23 +0300

    Composite.

---
+++ src/main/java/multiscripter/patterns/structural/Composite.java
@@ -0,0 +1,79 @@
+package multiscripter.patterns.structural;
+
+import java.util.HashMap;
+
+/**
+ * Компоновщик (Composite) — структурный шаблон проектирования.
+ * Решает задачу единого взаимодействия как с простыми,
+ * так и с составными (содержащими коллекцию простых) компонентами системы через
+ * унифицированный интерфейс. Объединяет объекты в древовидную структуру
+ * для представления иерархии от частного к целому.
+ */
+
+/**
+ * Пример.
+ * Дано:
+ * Сеть из компьютеров (древовидная структура), которая состоит из простых
+ * компонентов - компьютеров (листьев дерева (Leaf))
+ * и составных компонетов - серверов (узлов дерева (Composite)).
+ * Компонент имеет собстенную нагрузку = 1.
+ * Найти:
+ * Суммарную нагрузку компонента.
+ */
+
+/**
+ * Общий интерфейс для всех компонентов.
+ * Component.
+ */
+interface INet {
+
+  String getId();
+
+  /**
+   * Возвращает нагрузку элемента сети (Server или Host).
+   * @return нагрузка элемента сети.
+   */
+  int getLoad();
+}
+
+/**
+ * Компьютер в сети.
+ * Leaf (Простой компонент).
+ */
+class Host implements INet {
+
+  protected String id;
+
+  public Host(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public int getLoad() {
+    return 1;
+  }
+}
+
+/**
+ * Сервер с подключёнными к нему компьютерами в сети.
+ * Composite (Составной компонент).
+ */
+class Server extends Host {
+
+  HashMap<String, INet> nodes = new HashMap<>();
+
+  public Server(String id) {
+    super(id);
+  }
+
+  public void addNode(INet node) {
+    this.nodes.put(node.getId(), node);
+  }
+
+  public int getLoad() {
+    return this.nodes.values().stream().mapToInt(INet::getLoad).sum() + 1;
+  }
+}
---
+++ src/test/java/multiscripter/patterns/structural/CompositeTest.java
@@ -0,0 +1,48 @@
+package multiscripter.patterns.structural;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Компоновщик (Composite) — структурный шаблон проектирования.
+ * Решает задачу единого взаимодействия как с простыми,
+ * так и с составными (содержащими коллекцию простых) компонентами системы через
+ * унифицированный интерфейс. Объединяет объекты в древовидную структуру
+ * для представления иерархии от частного к целому.
+ */
+
+/**
+ * Пример.
+ * Дано:
+ * Сеть из компьютеров (древовидная структура), которая состоит из простых
+ * компонентов - компьютеров (листьев дерева (Leaf))
+ * и составных компонетов - серверов (узлов дерева (Composite)).
+ * Компонент имеет собстенную нагрузку = 1.
+ * Найти:
+ * Суммарную нагрузку компонента.
+ */
+public class CompositeTest {
+
+  @Test
+  public void showCompositeUsage() {
+    Server s1 = new Server("Host-1");
+    Server s2 = new Server("Host-2");
+    Server s3 = new Server("Host-3");
+    Server s4 = new Server("Host-4");
+
+    s1.addNode(s2);
+    s1.addNode(new Host("Host-5"));
+    s1.addNode(s3);
+    s2.addNode(new Host("Host-6"));
+    s2.addNode(new Host("Host-7"));
+    s3.addNode(s4);
+    s3.addNode(new Host("Host-8"));
+    s4.addNode(new Host("Host-9"));
+
+    // Суммарная нагрузка всей сети.
+    assertEquals(9, s1.getLoad());
+    // Суммарная нагрузка конкретного составного компонента (сервера).
+    assertEquals(3, s2.getLoad());
+  }
+}
